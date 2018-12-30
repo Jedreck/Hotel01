946a5ccff5e92eecf4f45943046a9ad9e70e6a5c
Jedreck <a302720001@qq.com>
2018-12-30 17:56:25 +0800

    mybatis使用db.properties配置 ----by jedreck

--- src/sample/test.java
+++ src/sample/test.java
@@ -1,6 +1,7 @@
 package sample;
 
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import sample.mapper.Customer;
 import sample.mapper.CustomerIFS;
@@ -55,17 +56,20 @@ public class test {
         //打印
         System.out.println(customer);
 
-        customer.setC_ID("145154199909203598");
+        //插入
+        /*customer.setC_ID("145154199909203598");
         customer.setC_phone("13846535684");
         System.out.println("插入：\n" + customer);
 
         int success = customerIFS.insertCustomer(customer);
-        session.commit();
+        session.commit();*/
 
+        //模糊查询
         List<Customer> list = customerIFS.selectCustomerByFuzzyName("涛");
-        System.out.println(list);
+        Logger.getRootLogger().info("RESULT--"+list);
 
         //关闭资源
         session.close();
+        System.out.println("123123");
     }
 }
