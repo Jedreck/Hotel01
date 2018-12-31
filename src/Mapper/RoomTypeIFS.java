94a624a46790b80a86380e4d20614787b3a0c5df
jinmutao <2549528898@qq.com>
2018-12-31 18:52:18 +0800

    写了添加房间类型的Servlet

--- src/Mapper/RoomTypeIFS.java
+++ src/Mapper/RoomTypeIFS.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface RoomTypeIFS {
     public int insertRoomType(RoomType roomType)throws IOException;
-    public List<RoomType> selectAllRoomType()throws IOException;
+    public List<RoomType> selectAllRoomType(int offset)throws IOException;
+    public int deleteRoomType(int R_roomtype)throws IOException;
 }
